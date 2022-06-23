915c6fd3b67110b112f6003c8f7e5c49ce1ce974
RichardKim <hiddeneventor@gmail.com>
2022-06-23 17:59:29 +0900

    v-model, reactive 학습

---
+++ src/lect/App-v-memo.vue
@@ -0,0 +1,29 @@
+<template>
+	<div v-memo="[subcribers]">
+		<p>subcribers: {{ subcribers }}</p>
+		<p>views: {{ views }}</p>
+		<p>likes: {{ likes }}</p>
+	</div>
+	<button @click="subcribers++">Sub++</button>
+	<button @click="views++">views++</button>
+	<button @click="likes++">likes++</button>
+	<div>
+		<p>subcribers: {{ subcribers }}</p>
+		<p>views: {{ views }}</p>
+		<p>likes: {{ likes }}</p>
+	</div>
+</template>
+
+<script>
+import { ref } from 'vue';
+
+export default {
+	setup() {
+		const subcribers = ref(4000);
+		const views = ref(400);
+		const likes = ref(20);
+
+		return { subcribers, views, likes };
+	},
+};
+</script>
