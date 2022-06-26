49e39ff66d32ea35f38b99693b7c14a02d1d3e36
RichardKim <hiddeneventor@gmail.com>
2022-06-26 15:18:31 +0900

    sloat 사용

---
+++ src/components/PostCreate.vue
@@ -0,0 +1,13 @@
+<template>
+	<div>
+		<button class="btn btn-primary">button</button>
+	</div>
+</template>
+
+<script>
+export default {
+	setup() {
+		return {};
+	},
+};
+</script>
--- src/lect/component.vue
+++ src/lect/component.vue
@@ -2,22 +2,17 @@
 	<div>
 		<TheNav></TheNav>
 		<TheView></TheView>
-		<hr />
-		<FancyButton>Click <span style="color: red">@@</span> </FancyButton>
-		<FancyButton></FancyButton>
 	</div>
 </template>
 
 <script>
 import TheNav from './components/TheNav.vue';
 import TheView from './components/TheView.vue';
-import FancyButton from './components/FancyButton.vue';
 
 export default {
 	components: {
 		TheNav,
 		TheView,
-		FancyButton,
 	},
 	setup() {
 		return {};
